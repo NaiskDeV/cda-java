55bfa50bd9254eebf95db01a8053ef55672e855d
NaiskDeV <54291577+NaiskDeV@users.noreply.github.com>
2019-11-04 12:51:34 +0100

    Update Tp2Exo1.java

--- tp2/Tp2Exo1.java
+++ tp2/Tp2Exo1.java
@@ -1,1 +1,22 @@
 
+package tp2;
+
+import java.util.Scanner;
+
+public class Tp2Exo1 {
+
+	public static void main(String[] args) {
+		Scanner lecture_clavier = new Scanner(System.in);
+		
+		int valeur1, valeur2, somme, produit;
+		System.out.print("Saisir un premier entier : ");
+		valeur1 = lecture_clavier.nextInt();
+		System.out.print("Saisir un second entier : ");
+		valeur2 = lecture_clavier.nextInt();
+		somme=valeur1+valeur2;
+		produit= valeur1*valeur2;
+		System.out.println("La somme de "+valeur1+ " et "+valeur2+" est : "+somme);
+		System.out.print("Le produit de "+valeur1+ " et "+valeur2+" est : "+produit);
+		lecture_clavier.close();
+
+	}
