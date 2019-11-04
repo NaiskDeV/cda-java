a7fd0792f186f6803273345f9fc5e6f24aa32ba1
NaiskDeV <54291577+NaiskDeV@users.noreply.github.com>
2019-11-04 17:55:42 +0100

    Create Tp4Exo4.java

---
+++ tp4/Tp4Exo4.java
@@ -0,0 +1,34 @@
+/*Nom du répertoire : TP4
+Nom du projet : Tp4Exo4
+Nom de la classe : Tp4Exo4
+Auteur : Laure */
+
+package tp4;
+import java.util.Scanner;
+
+public class Tp4Exo4 {
+
+	public static void main(String[] args) {
+
+		Scanner clavier = new Scanner(System.in);
+		
+		System.out.print("Quel nombre doit être testé?");
+		int n = clavier.nextInt();
+		boolean flag= true;
+		
+		for (int i=2; i<n/2; i++) {
+			if (n%i==0) {
+				flag=false;
+			}
+		}
+		
+		if (flag) {
+			System.out.print(n+" est premier");
+		}
+		else {
+			System.out.print(n+" n'est pas premier");
+		}
+
+	}
+
+}
